1d7c5ca6999d6cf5d8d3efecac505d57c1963cda
Mouse Reeve <mousereeve@riseup.net>
2018-10-31 10:29:48 -0700

    Remove generator and fix tokenizer for whole rests

--- parser.py
+++ parser.py
@@ -1,9 +1,8 @@
 ''' infinite music from a midi file, mostly if that midi is gnossiennes no 1 '''
-from mido import MidiFile, MidiTrack, Message
+from mido import MidiFile
 
 from collections import defaultdict
 import json
-import random
 import sys
 
 class NotationDistribution(object):
@@ -17,6 +16,8 @@ class NotationDistribution(object):
         self.track_dists = []
         # relationship between simulatanous notes between tracks
         self.joint_dist = defaultdict(lambda: defaultdict(lambda: 0))
+
+        self.measure_length = 1920
 
         # populates the distributions
         self.parse_midi(filename)
@@ -32,7 +33,7 @@ class NotationDistribution(object):
         # probability that one note follows another in a track
         for (idx, track) in enumerate(tracks):
             dist = defaultdict(lambda: defaultdict(lambda: 0))
-            tokens = tokenize_track(track)
+            tokens = self.tokenize_track(track)
             for i in range(len(tokens) - 1):
                 one = tokens[i]['identifier']
                 two = tokens[i + 1]['identifier']
@@ -52,99 +53,70 @@ class NotationDistribution(object):
 
         # probability of a note in a track given the behavior of the other track
 
-    def generate_music(self, time=(0.5 * 60 * 1000)):
-        ''' produce new midi data from the distributions
-        this should probably be re-written browser-side '''
-        self.starts.reverse()
-        mid = MidiFile()
-        json_data = []
-        for dist in self.track_dists:
-            # create a new track for the generated music
-            track = MidiTrack()
-            mid.tracks.append(track)
 
-            # create a text version
-            json_track = []
+    def tokenize_track(self, track):
+        ''' group 4/4 measures together (1920 ms) -- sorry satie '''
+        # controls and settings get added back later
+        notes = [n for n in track if n.type == 'note_on']
 
-            # select the same first note as the original piece
-            start = self.starts.pop()
-            token = start
-            count = 0
-            while count < time:
-                for note in token.split('|'):
-                    note = Message.from_str(note)
-                    count += note.time
-                    track.append(note)
-                    json_track.append({
-                        'note': note.note,
-                        'velocity': note.velocity,
-                        'time': note.time,
-                    })
-                options = dist[token]
-                try:
-                    token = weighted_choice(options)
-                except IndexError:
-                    print('no followup found for token: %s' % token)
-                    token = start
-            json_data.append(json_track)
-        mid.save('new.mid')
-        json.dump(json_data, open('new.json', 'w'))
+        tokens = []
+        group = []
+        running_time = 0
+        rest_token = {
+            'identifier': '70/1/0/1920',
+            'notes': [],
+        }
+        for note in notes:
+            running_time += note.time
+            # we're done, process the note group into a token
+            if running_time >= self.measure_length and note.velocity > 0:
+                add_rest = False
+                # we have a stray full measure rest
+                if group[-1][0].time > 3000:
+                    with_rest = group[-1]
+                    with_rest[0].time -= self.measure_length
+                    with_rest[1] -= self.measure_length
+                    group[-1] = with_rest
 
+                    with_rest = group[-2]
+                    with_rest[2] -= self.measure_length
+                    group[-2] = with_rest
+                    add_rest = True
 
-def tokenize_track(track):
-    ''' group 4/4 measures together (1920 ms) -- sorry satie '''
-    # controls and settings get added back later
-    notes = [n for n in track if n.type == 'note_on']
+                identifier = []
+                for n in group:
+                    duration = n[2] - n[1]
+                    if not n[0].velocity > 0:
+                        duration = 0
+                    identifier.append('%d/%d/%d/%d' % \
+                            (n[0].note, n[0].velocity, n[0].time, duration))
+                identifier = '|'.join(identifier)
+                token = {
+                    'notes': group,
+                    'identifier': identifier
+                }
+                tokens.append(token)
+                if add_rest:
+                    tokens.append(rest_token)
+                group = []
+                running_time = note.time
 
-    tokens = []
-    group = []
-    running_time = 0
-    for note in notes:
-        running_time += note.time
-        # we're done, process the note group into a token
-        if running_time >= 1920 and note.velocity > 0:
-            identifier = []
-            for n in group:
-                duration = n[2] - n[1]
-                if n[0].velocity > 0:
-                    print(n[0].note, n[1], n[2], duration)
-                else:
-                    duration = 0
-                identifier.append('%d/%d/%d/%d' % \
-                        (n[0].note, n[0].velocity, n[0].time, duration))
-            identifier = '|'.join(identifier)
-            token = {
-                'notes': group,
-                'identifier': identifier
-            }
-            tokens.append(token)
-            group = []
-            running_time = 0
+            # and let's get on with the next group
+            note.velocity = 100 if note.velocity else 0
 
-        # and let's get on with the next group
-        note.velocity = 100 if note.velocity else 0
+            note = [note, running_time, 0] # note, start time, end time
 
-        note = [note, running_time, 0] # note, start time, end time
-        if note[0].velocity == 0:
-            # we have to search backwards in the group to find the start
-            for (idx, n) in enumerate(group[::-1]):
-                if n[0].note == note[0].note:
-                    group[len(group) - 1 - idx][2] = running_time
-                    break
-        group.append(note)
+            # a note ender
+            if note[0].velocity == 0:
+                # we have to search backwards in the group to find the start
+                for (idx, n) in enumerate(group[::-1]):
+                    if n[0].note == note[0].note:
+                        group[len(group) - 1 - idx][2] = running_time
+                        break
+            group.append(note)
 
-    return tokens
+        return tokens
 
-
-def weighted_choice(options):
-    ''' prefer likelier followups '''
-    if len(options) == 1:
-        return options.keys()[0]
-
-    weighted = []
-    for (option, weight) in options.items():
-        weighted += [option] * weight
-    return random.choice(weighted)
 
 
 if __name__ == '__main__':
@@ -154,5 +126,4 @@ if __name__ == '__main__':
         f = 'gnossiennes_1.mid'
 
     notation = NotationDistribution(f)
-    import pdb;pdb.set_trace()
-    #notation.generate_music()
+    json.dump(notation.track_dists, open('dists.json', 'w'))
