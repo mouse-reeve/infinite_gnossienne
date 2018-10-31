ce0dee8e6573e78f53a68ab1e86eb3cc089cc50c
Mouse Reeve <mousereeve@riseup.net>
2018-10-31 11:18:54 -0700

    Adds secret bar for whole notes; flats not sharps

--- static/js/play.js
+++ static/js/play.js
@@ -8,13 +8,17 @@ window.onload = function () {
     var context = renderer.getContext();
     var staves = [
         new VF.Stave(10, 40, 400),
-        new VF.Stave(10, 100, 400),
+        new VF.Stave(10, 150, 400),
+        // the secret second bass clef where the whole notes live
+        new VF.Stave(10, 150, 400),
     ];
 
-    var clefs = ['treble', 'bass'];
+    var clefs = ['treble', 'bass', 'bass'];
     for (var i = 0; i < staves.length; i++) {
-        staves[i].addClef(clefs[i]);
-        staves[i].setContext(context).draw();
+        if (clefs[i]) {
+            staves[i].addClef(clefs[i]);
+            staves[i].setContext(context).draw();
+        }
     }
 
 
@@ -72,19 +76,22 @@ window.onload = function () {
             var options = dists[track_id][token];
             token = weighted_random(options);
 
-            window.setTimeout(playMeasure.bind(null, token, track_id), 1920 * tempo_modifier);
+            //window.setTimeout(playMeasure.bind(null, token, track_id), 1920 * tempo_modifier);
         };
 
         playMeasure(starts[0], 0);
-        //playMeasure(starts[1], 1);
+        playMeasure(starts[1], 1);
     });
 
     function drawNotes(notes, staff) {
+        //console.log(notes);
         var vf_notes = [];
+        var vf_notes_w = [];
 
         var grace_note;
+        var first_note = true;
         for (var i = 0; i < notes.length; i++) {
-            // --- check for rests
+            // ------------ check for rests
             if (notes[i] == '70/1/0/1920') {
                 // special case: whole note rest
                 vf_notes.push(
@@ -93,25 +100,48 @@ window.onload = function () {
                 break;
             }
             note = notes[i].split('/');
-            if (i === 0 && note[2] > 2 && note[3] > 60) {
+            if (first_note && note[2] > 25 && note[3] > 60) {
                 // we have ourselves a rest
                 var rest = get_duration(note[2] - 50) + 'r';
                 vf_notes.push(
                     new VF.StaveNote({clef: clefs[staff], keys: ['b/4'], duration: rest })
                 );
+                //console.log(rest);
             }
 
+            // ------------- note info
             var note_name = midi_to_note(note[0]);
             var duration = parseInt(note[3]);
+            var type = get_duration(duration);
+            if (duration <= 0) {
+                continue;
+            }
+            //console.log(type);
+
+            // ------------- grace notes
             // stash the grace note away to add it as a modifier to the next note
             if (duration < 100) {
                 var gn = new Vex.Flow.GraceNote({keys: [note_name], duration: '16', slash: true });
                 grace_note = new Vex.Flow.GraceNoteGroup([gn], true);
                 continue;
             }
-            var type = get_duration(duration);
 
-            var vf_note = new VF.StaveNote({clef: clefs[staff], keys: [note_name], duration: type });
+            // ------------ chords
+            var names = [note_name];
+            while (true) {
+                var proposed = notes[i + 1] ? notes[i + 1].split('/') : undefined;
+                if (!proposed || proposed[2] > 0 || proposed[1] === 0) {
+                    break;
+                }
+                i += 1;
+                var proposed_name = midi_to_note(proposed[0]);
+                names.push(proposed_name);
+            }
+            //console.log(names);
+
+            // ------------ create and render vexflow notes
+            var vf_note = new VF.StaveNote({clef: clefs[staff], keys: names, duration: type });
+            // modifiers
             if (grace_note) {
                 vf_note.addModifier(0, grace_note.beamNotes());
                 grace_note = undefined;
@@ -119,14 +149,28 @@ window.onload = function () {
             if (type == 'hd') {
                 vf_note.addDotToAll();
             }
-            vf_notes.push(vf_note);
+            // TODO: accidentals
+            // handle satie doubling up the bass stave
+            if (type == 'w') {
+                vf_notes_w.push(vf_note);
+            } else {
+                vf_notes.push(vf_note);
+                first_note = false;
+            }
         }
 
         var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
         voice.addTickables(vf_notes);
         var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
         voice.draw(context, staves[staff]);
+        if (vf_notes_w.length > 0) {
+            voice = new VF.Voice({num_beats: 4,  beat_value: 4});
+            voice.addTickables(vf_notes_w);
+            formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+            voice.draw(context, staves[2]);
+        }
     }
+    //drawNotes(["72/100/480/239", "72/0/239/0", "75/100/1/239", "75/0/239/0", "74/100/1/479", "74/0/479/0", "72/100/1/479", "72/0/479/0"]);
 
 };
 
@@ -146,7 +190,7 @@ function get_duration(duration) {
 
 function midi_to_note(midi) {
     var octave = Math.round(midi / 12) - 1;
-    var note = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][midi % 12];
+    var note = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B'][(midi % 12)];
     return note + '/' + octave;
 }
 
