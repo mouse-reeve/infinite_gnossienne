7ae34ab1fe817cc8f6a55f72d09d91a6c059bfa5
Mouse Reeve <mousereeve@riseup.net>
2018-11-01 17:34:39 -0700

    Remove existing minified js before rebuilding

--- minify.sh
+++ minify.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-java -jar ../closure-compiler/closure-compiler-v20181008.jar --js ../gnossiennes/static/js/*.js --js_output_file static/js/gnossiennes.min.js
+rm static/js/gnossiennes.min.js
+java -jar ../closure-compiler/closure-compiler-v20181008.jar --js static/js/*.js --js_output_file static/js/gnossiennes.min.js
